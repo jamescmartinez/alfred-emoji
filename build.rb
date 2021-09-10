7ef6e06ddbad20feefea5af0d68a15a69aab9086
James Martinez <jamescmartinez@gmail.com>
2021-09-10 14:51:54 -0500

    Clean up build script

--- build.rb
+++ build.rb
@@ -1,86 +1,59 @@
-require 'fileutils'
-require 'json'
-require 'net/http'
-require 'securerandom'
-require 'zip'
+require "fileutils"
+require "json"
+require "net/http"
+require "securerandom"
+require "zip"
 
-class Emoji
-  attr_accessor :emoji, :name, :shortcode
-
-  def initialize(emoji, name)
-    @emoji = emoji
-    @name = name.tr('_', ' ')
-    @shortcode = ":#{@name}:"
-  end
-
-  def to_alfredsnippet
-    AlfredSnippet.new(emoji, SecureRandom.uuid, name, shortcode)
-  end
-
-  def self.all
-    url = 'https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json'
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    json = JSON.parse(response)
-
-    json.select { |g| g.key?('emoji') }.flat_map do |e|
-      e['aliases'].map do |a|
-        Emoji.new(e['emoji'], a)
-      end
-    end
-  end
+def fetch_gemoji
+  url = "https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)
 end
 
-class AlfredSnippet
-  attr_accessor :snippet, :uid, :name, :keyword
+def map_gemoji_to_alfredsnippets(gemoji)
+  gemoji.flat_map do |g|
+    g["aliases"].map do |a| # e.g. "smiley"
+      emoji = g["emoji"] # e.g. "😃"
+      keyword = ":#{a.tr('_', ' ')}:" # e.g. ":smiley:"
+      tags = g["tags"].join(", ") # e.g. "happy, joy, haha"
+      name = "#{emoji} - #{keyword}" # e.g. "😃 - :smiley:"
+      name += " - #{tags}" if g["tags"].count.positive? # e.g. "😃 - :smiley: - happy, joy, haha"
 
-  def initialize(snippet, uid, name, keyword)
-    @snippet = snippet
-    @uid = uid
-    @name = name
-    @keyword = keyword
-  end
-
-  def to_json
-    {
-      alfredsnippet: {
-        snippet: @snippet,
-        uid: @uid,
-        name: "#{@snippet} - #{@name}",
-        keyword: @keyword
+      {
+        alfredsnippet: {
+          uid: name,
+          snippet: emoji,
+          name: name,
+          keyword: keyword
+        }
       }
-    }.to_json
-  end
-end
-
-class AlfredSnippets
-  def initialize(alfredsnippets)
-    @alfredsnippets = alfredsnippets
-  end
-
-  def write
-    FileUtils.mkdir_p('tmp')
-    write_to_tmp
-    create_alfredsnippets_file
-    FileUtils.rm_rf('tmp')
-  end
-
-  private
-
-  def write_to_tmp
-    @alfredsnippets.each do |alfredsnippet|
-      filename = "#{alfredsnippet.name} #{alfredsnippet.uid}.json"
-      File.write("tmp/#{filename}", alfredsnippet.to_json)
-    end
-  end
-
-  def create_alfredsnippets_file
-    Zip::File.open('Emoji.alfredsnippets', Zip::File::CREATE) do |zipfile|
-      Dir[File.join('tmp', '*')].each do |file|
-        zipfile.add(file.sub('tmp/', ''), file)
-      end
     end
   end
 end
 
-AlfredSnippets.new(Emoji.all.map(&:to_alfredsnippet)).write
+def write_alfredsnippets_file(alfredsnippets)
+  FileUtils.mkdir_p("tmp")
+
+  alfredsnippets.each do |alfredsnippet|
+    filename = "#{alfredsnippet.dig(:alfredsnippet, :name)}.json"
+    File.write("tmp/#{filename}", alfredsnippet.to_json)
+  end
+
+  FileUtils.rm_rf("Emoji.alfredsnippets")
+
+  Zip::File.open("Emoji.alfredsnippets", Zip::File::CREATE) do |zipfile|
+    Dir[File.join("tmp", "*")].each do |file|
+      zipfile.add(file.sub("tmp/", ""), file)
+    end
+  end
+
+  FileUtils.rm_rf("tmp")
+end
+
+#####
+
+gemoji = fetch_gemoji
+alfredsnippets = map_gemoji_to_alfredsnippets(gemoji)
+write_alfredsnippets_file(alfredsnippets)
+puts "Done! #{alfredsnippets.count} Alfred emoji snippets created."
